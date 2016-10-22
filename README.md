9c2b58cff383f11ab2a06cdcba9e3917634cfa05
syoamakase <syoamakase@gmail.com>
2016-10-22 23:15:53 +0900

    add script to control turtlebot

--- README.md
+++ README.md
@@ -2,12 +2,14 @@
 
 
 
-## add function of syoamakase
+## add function of ueno
 
 - make a gazebo worldfile
 
 - make rospackage to call a above worldfile
 
-- make python scripts to control turtlebot and to see ball location.
+- make a python script to control turtlebot(/environments/gazebo_action.py)
+
+- make a python script to see ball location
 
 
---
+++ susanoh/environments/gazebo_action.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import rospy
+from geometry_msgs.msg import Twist
+
+class Gazebo_Action():
+    def __init__(self, robot_name="mobile_base"):
+    	# rostopic name for turtlebot
+        topic_name = robot_name+'command/velocity'
+        # they must be called
+        rospy.init_node(robot_name+'vel_publisher')
+        self.pub = rospy.Publisher(topic_name, Twist, queue_size=10)
+
+    '''
+    action(Int) define robot action
+    0: robot don't move.
+    1: robot move fowrard
+    2:            backward
+    3: robot rotate right
+    4:              left
+    '''
+    def control_action(self,action):
+        if action == 0:
+            self.move_to_neutral()
+        elif action == 1:
+            self.move_forward()
+        elif action == 2:
+            self.move_backword()
+        elif action == 3:
+            self.rotate_right()
+        elif action == 4:
+            self.rotate_left()
+        else:
+            rospy.loginfo("unexpected action=%d.",action)
+            self.move_to_neutral()
+
+
+    def move_to_neutral(self):
+        vel = Twist()
+        vel.linear.x = 0
+        vel.linear.y = 0
+        vel.linear.z = 0
+        vel.angular.x = 0
+        vel.angular.y = 0
+        vel.angular.z = 0
+
+        self.pub.publish(vel)
+
+
+    def move_forward(self):
+        vel = Twist()
+        vel.linear.x = 1.0
+        self.pub.publish(vel)
+        # rospy.sleep(7.0)
+
+
+    def move_backword(self):
+        vel = Twist()
+        vel.linear.x = -1.0
+        self.pub.publish(vel)
+
+
+    def rotate_right(self):
+        vel = Twist()
+        vel.angular.z = 2.0
+        self.pub.publish(vel)
+
+
+    def rotate_left(self):
+        vel = Twist()
+        vel.angular.z = -2.0
+        self.pub.publish(vel)
