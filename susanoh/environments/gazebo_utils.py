605287c3c65056e9413fe6436751ebea70cefde8
syoamakase <syoamakase@gmail.com>
2016-11-06 21:21:16 +0900

    enable to multi robots

--- susanoh/environments/gazebo_utils.py
+++ susanoh/environments/gazebo_utils.py
@@ -33,7 +33,7 @@ def get_ball_location(number=0):
 
 
 # reset gazebo world(it resets models)
-def reset_world(robot_x=0,robot_y=0,robot_angle=0,ball_x=1,ball_y=0):
+def reset_world(robot_x=3.0,robot_y=0,robot_angle=0,ball_x=3.25,ball_y=0):
     rospy.wait_for_service('gazebo/reset_world')
     # ServiceProxy and call means `rosservice call /gazebo/reset_world`
     srv = rospy.ServiceProxy('gazebo/reset_world', std_srvs.srv.Empty)
@@ -41,14 +41,13 @@ def reset_world(robot_x=0,robot_y=0,robot_angle=0,ball_x=1,ball_y=0):
 
     # set the robot
     model_pose = Pose()
-    #model_pose.position.x = robot_x
-    model_pose.position.x = 3.0
-    #model_pose.position.y = robot_y
+    model_pose.position.x = robot_x
+    # model_pose.position.x = 3.0
     model_pose.position.y = robot_y
     model_pose.orientation.z = np.sin(robot_angle/2.0) 
     model_pose.orientation.w = np.cos(robot_angle/2.0) 
     modelstate = ModelState()
-    modelstate.model_name = 'mobile_base'
+    modelstate.model_name = 'mobile_base_player_1'
     modelstate.reference_frame = 'world'
     modelstate.pose = model_pose
     set_model_srv = rospy.ServiceProxy('gazebo/set_model_state', SetModelState)
@@ -57,9 +56,8 @@ def reset_world(robot_x=0,robot_y=0,robot_angle=0,ball_x=1,ball_y=0):
 
     # set the ball
     model_pose = Pose()
-    # model_pose.position.x = ball_x
-    model_pose.position.x = 3.25
-    # model_pose.position.y = ball_y
+    model_pose.position.x = ball_x
+    # model_pose.position.x = 3.25
     model_pose.position.y = ball_y
     modelstate = ModelState()
     modelstate.model_name = 'soccer_ball'
@@ -85,7 +83,7 @@ def reset_simulation(robot_x=0,robot_y=0,robot_angle=0,ball_x=1,ball_y=0):
     model_pose.orientation.z = np.sin(robot_angle/2.0) 
     model_pose.orientation.w = np.cos(robot_angle/2.0) 
     modelstate = ModelState()
-    modelstate.model_name = 'mobile_base'
+    modelstate.model_name = 'mobile_base_player_1'
     modelstate.reference_frame = 'world'
     modelstate.pose = model_pose
     set_model_srv = rospy.ServiceProxy('gazebo/set_model_state', SetModelState)
