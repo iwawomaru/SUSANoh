57364e44f200ad99afa3080e6763676bbbcf2bd9
芦原佑太 <asupara@ashiharayuufutoshi-no-MacBook-Pro.local>
2016-11-09 16:31:20 +0900

    add read_model

--- examples/dqn_test.py
+++ examples/dqn_test.py
@@ -6,6 +6,8 @@
 
 
 import sys,os
+from chainer import serializers
+import argparse
 
 # for Python3 style
 import six
@@ -16,6 +18,13 @@ import numpy as np
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
 from susanoh import components, environments
 
+
+parser = argparse.ArgumentParser(description='SUSANoh:')
+parser.add_argument('--initmodel', '-m', default='',
+                   help='Initialize the model from given file')
+args = parser.parse_args()
+
+
 if __name__ == '__main__':
 
     # Change to your environment
@@ -25,10 +34,18 @@ if __name__ == '__main__':
     # setup model
     # If you want to use L1 regularization, put the rate into 'L1_rate'.
     model = components.DQN(n_stat, n_act, L1_rate=None, on_gpu=True)
+    if args.initmodel:
+        print('Load model from', args.initmodel)
+        serializers.load_npz(args.initmodel, model.agent.q)
 
     # Change to your environment
     env = environments.SoccerEnv(model)
-
+    i = 0
     while True:
         env.execute()
+
+        i = i + 1
+        if i % 10 == 0:
+           print 'save the model'
+           model.agent.save(index=i)
 
