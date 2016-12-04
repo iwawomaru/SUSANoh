a2e2edac31834582f3f9309cebb3ec0f0bade3a6
daiki_shimada_mba <sheema.sheemap@gmail.com>
2016-12-05 01:42:14 +0900

    Add Single Model Tester

---
+++ sig-agi/src/single_model_test.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import argparse
+import os
+import six
+import numpy as np
+import chainer
+from chainer import cuda, optimizers, serializers
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+# import dataset script
+import cifar10
+
+# import from Masalachai
+from masalachai import DataFeeder
+from masalachai import Logger
+from masalachai import trainers
+from masalachai import models
+
+# import model network
+import convnet
+
+
+# argparse
+parser = argparse.ArgumentParser(description='Convolutional Network Trainer on CIFAR-10')
+parser.add_argument('--batch', '-b', type=int, default=1000, help='batchsize (default: 1000)')
+parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU device #, if you want to use cpu, use -1 (default: -1)')
+args = parser.parse_args()
+
+
+def cifar_preprocess(data):
+    data['data'] /= 255.
+    return data
+
+# Configure GPU Device
+if args.gpu >= 0:
+    cuda.check_cuda_available()
+xp = cuda.cupy if args.gpu >= 0 else np
+
+# loading dataset
+dataset = cifar10.load()
+
+dim = dataset['test']['data'][0].size
+N_test = len(dataset['test']['target'])
+test_data_dict = {'data':dataset['test']['data'].astype(np.float32),
+                  'target':dataset['test']['target'].astype(np.int32)}
+test_data = DataFeeder(test_data_dict, batchsize=args.batch)
+test_data.hook_preprocess(cifar_preprocess)
+
+# Model Setup
+model_file = 'shallow_a.h5'
+model_name = 'shallow'
+model = models.ClassifierModel(convnet.models[model_name]())
+serializers.load_hd5(f, model)
+if args.gpu >= 0:
+    cuda.get_device(args.gpu).use()
+    model.to_gpu()
+
+res = []
+for idx in six.moves.range(0, len(test_data_dict['data']), args.batch):
+    # prepare batch
+    data_dict = {
+            'data': train_data_dict['data'][idx:idx+args.batch], 
+            'target': train_data_dict['target'][idx:idx+args.batch]
+            }
+    data_list = [{k : v[i] if getattr(v,'__iter__',False) else v
+        for k,v in data_dict.items()} for i in six.moves.range(len(data_dict['data']))]
+    dict_list = [cifar_preprocess(d) for d in data_list]
+    vx = tuple( [chainer.Variable( xp.asarray([d['data'] for d in dict_list]) ) ] )
+
+    outputs = xp.asnumpy(model.predict(vx).data)
+    res.extend(list(outputs))
+
+print('Accuracy: ', accuracy_score(test_dict['target'], res))
+print('Precision: ', precision_score(test_dict['target'], res))
+print('Recall: ', recall_score(test_dict['target'], res))
+
